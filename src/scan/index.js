9d926a19b3138e34c871a059ed94143bf030f886
kerdamon <kwalas314@gmail.com>
2022-12-17 02:51:34 +0000

    Add pseudocode for scan function

--- src/scan/index.js
+++ src/scan/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const app = express();
 app.use('/:id', (req, res) => {
-    res.send(req.params);
+    const id = req.params.id;
+    // get redirection addres based on id
+    // redirect to address
+    res.redirect('https://google.com');
 });
 
 exports.qrcode = app;
