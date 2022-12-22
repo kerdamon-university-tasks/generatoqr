e7bc987658062928d4e907898876c09aa3fb731e
kerdamon <kwalas314@gmail.com>
2022-12-22 01:38:19 +0000

    Implement scan function

--- src/crud/index.js
+++ src/crud/index.js
@@ -1,5 +1,17 @@
-const functions = require('@google-cloud/functions-framework');
+const express = require('express');
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
-functions.http('helloHttp', (req, res) => {
- res.send(`Hello crud ${req.query.name || req.body.name || 'World'}!!!!`);
+initializeApp();
+const db = getFirestore();
+
+const app = express();
+app.use('/:id', async (req, res) => {
+    const id = req.params.id;
+    const destination_adress = db.collection('destination_adresses').doc(id);
+    const doc = await document.get();
+    res.send(doc)
+    // res.redirect(destination_adress.data());
 });
+
+exports.qrcode = app;
--- src/scan/index.js
+++ src/scan/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+
+initializeApp();
+const db = getFirestore();
 
 const app = express();
-app.use('/:id', (req, res) => {
+app.use('/:id', async (req, res) => {
     const id = req.params.id;
-    // get redirection addres based on id
-    // redirect to address
-    res.redirect('https://google.com');
+    const doc = await db.collection('destination_adresses').doc(id).get();
+    if (typeof doc.data() === 'undefined'){
+        res.status(404).send("Wrong id")        
+    }
+    const destination_address = doc.data()["destination"]
+    res.redirect(destination_address);
 });
 
 exports.qrcode = app;
